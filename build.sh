2f31ba6d1d8b3ebf03b5526641b28566f0cf2d68
John Woodell <woodie@netpress.com>
2019-07-02 23:01:24 -0700

    Use new emulator.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ME2=/Library/Java/microemulator-2.0.4
 JDK=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 JME=/Applications/Java_ME_SDK_3.0.app/Contents/Resources
 
@@ -7,7 +8,12 @@ CLDC_1_1=$JME/lib/cldc_1.1.jar
 MIDP_2_0=$JME/lib/midp_2.0.jar
 IO_CLASSES=$JME/lib/jsr75_1.0.jar
 
-APP=SudokuJ2ME
+DEVICE=$ME2/devices/microemu-device-large.jar
+APP_NAME=SudokuJ2ME
+
+VER=$(grep MIDlet-Version MANIFEST.MF | cut -d' ' -f 2)
+APP=$APP_NAME-$VER
+
 rm -rf tmpclasses classes dist
 mkdir -p tmpclasses classes dist
 
@@ -29,7 +35,9 @@ cd dist
 zip -r $APP.zip $APP.jad $APP.jar
 cd ..
  
+# https://code.google.com/archive/p/microemu/
 if [ -f dist/$APP.jad ]; then
-  $JME/bin/emulator -Xdevice:DefaultCldcPhone1 \
-      -Xdescriptor:dist/$APP.jad -Xdomain:maximum
+  $JDK/bin/java -cp $ME2/microemulator.jar:$DEVICE:dist/$APP.jar \
+      org.microemu.app.Main $APP_NAME --propertiesjad dist/$APP.jad \
+      --device org/microemu/device/large/device.xml
 fi
